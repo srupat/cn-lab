0eae05e425975ba89c81f4af9abce943af127f05
srupat <srujanpatwardhan@gmail.com>
2024-08-14 12:45:16 +0530

    manchester and diff manchester encodings added

--- encodings.py
+++ encodings.py
@@ -135,7 +135,7 @@ if __name__=='__main__':
     print("Enter the size of Encoded Data : ")
     size=int(input())
     li=[]
-    print('Enter the binary bits sequnce of length ',size,' bits : \n')
+    print('Enter the binary bits seqeunce of length ',size,' bits : \n')
     for i in range(size):
         li.append(int(input()))
     plot(li) 
